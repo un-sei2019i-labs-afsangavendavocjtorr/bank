fdf0b82f09b6435960c842e6fd94f6feec3bbb14
Gabriel <gabriel19oct@hotmail.com>
2019-05-22 18:12:18 -0500

    Archivo y clases base creadas

---
+++ test/src/myproject/Account.java
@@ -0,0 +1,30 @@
+package myproject;
+
+public class Account {
+	private int accountNumber;
+	private double balance;
+	
+	public Account (int accountNumber, int balance) {
+		this.balance = balance;
+		this.accountNumber = accountNumber;
+	}
+
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(int accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
+	
+	
+}
---
+++ test/src/myproject/Main.java
@@ -0,0 +1,27 @@
+package myproject;
+
+import java.util.ArrayList;
+
+public class Main {
+	private static ArrayList<Account> database;
+	
+	public static void main(String[] args) {
+		database = new ArrayList<>();
+	}
+	
+	
+	private static void createAccount() {
+		database.add(null);
+	}
+	
+	
+	private static void changeBalance() {
+		
+	}
+	
+	private static void deleteAccount() {
+		
+	}
+	
+	
+}
