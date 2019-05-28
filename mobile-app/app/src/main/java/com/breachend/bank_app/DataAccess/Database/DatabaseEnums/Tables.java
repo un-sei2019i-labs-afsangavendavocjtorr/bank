9023a5e2eae77fa257ecdabb097a27fafcb23060
jhonatan torres <yon.t.f@hotmail.com>
2019-05-27 23:58:00 -0500

    Presentation with logic

---
+++ mobile-app/app/src/main/java/com/breachend/bank_app/DataAccess/Database/DatabaseEnums/Tables.java
@@ -0,0 +1,16 @@
+package com.breachend.bank_app.DataAccess.Database.DatabaseEnums;
+
+public enum Tables {
+    Users(0);
+
+    private int value;
+
+    Tables(int value){
+        this.value = value;
+    }
+
+    public int Val(){
+        return value;
+    }
+
+}
