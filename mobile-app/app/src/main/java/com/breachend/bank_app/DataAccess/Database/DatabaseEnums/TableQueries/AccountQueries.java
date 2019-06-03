eee46e3ebf20ed4ec14d40fd2b2e2d4325097c0f
unknown <afsanchezsa@unal.edu.co>
2019-06-02 19:28:58 -0500

    se agregan todas las tablas al tipo enumerado Tables.java

---
+++ mobile-app/app/src/main/java/com/breachend/bank_app/DataAccess/Database/DatabaseEnums/TableQueries/AccountQueries.java
@@ -0,0 +1,9 @@
+package com.breachend.bank_app.DataAccess.Database.DatabaseEnums.TableQueries;
+
+import com.breachend.bank_app.DataAccess.Database.DatabaseEnums.Tables;
+
+public enum AccountQueries {
+
+
+
+}
--- mobile-app/app/src/main/java/com/breachend/bank_app/DataAccess/Database/DatabaseEnums/Tables.java
+++ mobile-app/app/src/main/java/com/breachend/bank_app/DataAccess/Database/DatabaseEnums/Tables.java
@@ -1,7 +1,12 @@
 package com.breachend.bank_app.DataAccess.Database.DatabaseEnums;
 
 public enum Tables {
-    Users("USERS");
+    Users("USERS"),
+     Account_Data("ACCOUNT_DATA"),
+    Administrator("ADMINISTRATOR"),
+    Transactions("TRANSACTIONS"),
+    Transaction_Type("TRANSACTION_TYPE"),
+    User_Password("USER_PASSWORD")  ;
 
     private String name;
 
