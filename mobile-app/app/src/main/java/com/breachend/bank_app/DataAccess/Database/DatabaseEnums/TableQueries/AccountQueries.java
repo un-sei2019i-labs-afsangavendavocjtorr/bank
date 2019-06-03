344b8d6aca438d92e93fdf2199c764613bd173a9
unknown <afsanchezsa@unal.edu.co>
2019-06-02 20:10:35 -0500

    se agrega el archivo AccountQueries.java

--- mobile-app/app/src/main/java/com/breachend/bank_app/DataAccess/Database/DatabaseEnums/TableQueries/AccountQueries.java
+++ mobile-app/app/src/main/java/com/breachend/bank_app/DataAccess/Database/DatabaseEnums/TableQueries/AccountQueries.java
@@ -1,25 +1,31 @@
 package com.breachend.bank_app.DataAccess.Database.DatabaseEnums.TableQueries;
 
+import com.breachend.bank_app.DataAccess.Database.DatabaseEnums.TableFields.AccountFields;
 import com.breachend.bank_app.DataAccess.Database.DatabaseEnums.Tables;
 
 public enum AccountQueries {
-    Create("CREATE TABLE "+Tables.Account_Data+
-            " ( AC_CODE bigint identity primary key," +
-                  "BALANCE  money null,"+
-           " ID_USER  bigint null)"
+    Create("CREATE TABLE "+Tables.Account_Data+"("+
+            AccountFields.AC_CODE.getKey()+" "+ AccountFields.AC_CODE.getDataType()+","+
+            AccountFields.BALANCE.getKey()+" "+AccountFields.BALANCE.getDataType()+","+
+            AccountFields.ID_USER.getKey()+" "+AccountFields.ID_USER.getDataType()+" );"
+    ),
+Drop("DROP TABLE IF EXISTS "+ Tables.Account_Data.getName() +";"),
+    SelectByIdUser("SELECT * FROM "+Tables.Account_Data+" where "+AccountFields.ID_USER.getKey()+"= %d;")
 
-
-
-
-
-    );
+    ;
     private String query;
     private AccountQueries(String query){
         this.query=query;
     }
+
+
+    public String getFormat(Object []args ){
+        return String.format(query,args);
+    }
     public String getQuery(){
         return this.query;
     }
+
 
 
 }
